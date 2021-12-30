65a2f78427b0bd0514a61acdcb722b93ba4c8a1f
NastyPigz <williamzwy06701@gmail.com>
2021-12-29 21:45:55 -0500

    ok

--- src/pages/free.tsx
+++ src/pages/free.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import React from 'react'
-import { useInterval } from './helpers/useInterval'
+import { useInterval } from '../helpers/useInterval'
 
 const Home: NextPage = () => {
   const [rotate, setRotate] = React.useState<number>(0);
