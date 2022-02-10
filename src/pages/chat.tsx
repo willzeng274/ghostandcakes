05111225b35f2a6988fc38860a75aace32c3f0ee
PullStackPigDev <s300099227@ddsbstudent.ca>
2022-02-09 19:32:40 -0500

    fb

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,118 +1,128 @@
-import React, { useEffect, useState } from "react";
-import Pusher from "pusher-js";
-import axios from "axios";
-import { useRouter } from "next/router";
+import React, { useRef, useState } from 'react';
 
-const SendMessage = ({ handleSubmit, handleMessageChange, message}: any) => {
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={message}
-        onChange={handleMessageChange}
-        placeholder="start typing...."
-      />
-      <button
-        type="submit"
-      >
-        Send
-      </button>
-    </form>
-  );
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const clientCredentials = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASEURL
 };
 
-const Chat = () => {
-  const router = useRouter();
-  const [chats, setChats] = useState<Array<any>>([]);
-  const [sender, setSender] = useState<string>("");
-  const [messageToSend, setMessageToSend] = useState<string>("");
-  const pusher = React.useMemo(() => new Pusher(process.env.NEXT_PUBLIC_APP_KEY || "", {
-    cluster: process.env.NEXT_PUBLIC_CLUSTER
-  }), []);
-  useEffect(() => {
-    let usrname: string = prompt("Username?") || "User_" + String(Math.floor(Math.random() * 1000));
-    if (usrname.length > 10) {
-      usrname = usrname.substring(0, 10);
-    }
-    setSender(usrname);
-    const channel = pusher.subscribe("chat");
-    channel.bind("chat-event", function (data: any) {
-      console.log("Received event")
-      setChats((prevState): Array<any> => [
-        ...prevState,
-        { sender: data.sender, message: data.message },
-      ]);
-    });
-    console.log("Reloaded")
-    return () => {
-      pusher.unsubscribe("chat");
-    };
-  }, [pusher]);
+firebase.initializeApp(clientCredentials);
 
-  const handleSignOut = () => {
-    pusher.unsubscribe("chat");
-    router.push("/");
-  };
+const auth: any = firebase.auth();
+const firestore: any = firebase.firestore();
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    if (messageToSend.trim().length < 1) {
-      return;
-    }
-    await axios.post("/api/pusher", {
-      message: messageToSend,
-      sender,
-    });
-    setMessageToSend("");
-  };
+function App() {
+
+  const [user] = useAuthState(auth);
 
   return (
-    <div>
-      <div>
-        <div>
-          <div>
-            <div>
-              <p>
-                Hello, <span>{sender}</span>
-              </p>
-              <div>
-                <button
-                  onClick={handleSignOut}
-                >
-                  Sign out
-                </button>
-              </div>
-            </div>
+    <div className="App">
+      <header>
+        <SignOut />
+      </header>
 
-            <div>
-              <h2>You&apos;re online.</h2>
-            </div>
-          </div>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
 
-          <div>
-            CHAT BOX
-            <div style={{height: "70vh", width: "100%", overflowY: "scroll", border: "1px solid black"}}>
-              {chats.map((chat, id) => (
-                <div key={id}>
-                  <div>
-                    <b>{chat.sender === sender ? "You" : chat.sender}</b> : {chat.message.replaceAll("\\n", "\n")}
-                  </div>
-                </div>
-              ))}
-            </div>
-
-            <div>
-              <SendMessage
-                message={messageToSend}
-                handleMessageChange={(e: any) => setMessageToSend(e.target.value)}
-                handleSubmit={handleSubmit}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
-};
+}
 
-export default Chat;
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef<any>();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' } as any);
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e: any) => {
+    e.preventDefault();
+
+    const { uid, displayName } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      displayName
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>Send🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props: any) {
+  const { text, uid, displayName } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <b>{displayName}</b>
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
+
+export default App;
---
+++ src/pages/chat_legacy.tsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const SendMessage = ({ handleSubmit, handleMessageChange, message}: any) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={message}
+        onChange={handleMessageChange}
+        placeholder="start typing...."
+      />
+      <button
+        type="submit"
+      >
+        Send
+      </button>
+    </form>
+  );
+};
+
+const Chat = () => {
+  const router = useRouter();
+  const [chats, setChats] = useState<Array<any>>([]);
+  const [sender, setSender] = useState<string>("");
+  const [messageToSend, setMessageToSend] = useState<string>("");
+  const pusher = React.useMemo(() => new Pusher(process.env.NEXT_PUBLIC_APP_KEY || "", {
+    cluster: process.env.NEXT_PUBLIC_CLUSTER
+  }), []);
+  useEffect(() => {
+    let usrname: string = prompt("Username?") || "User_" + String(Math.floor(Math.random() * 1000));
+    if (usrname.length > 10) {
+      usrname = usrname.substring(0, 10);
+    }
+    setSender(usrname);
+    const channel = pusher.subscribe("chat");
+    channel.bind("chat-event", function (data: any) {
+      console.log("Received event")
+      setChats((prevState): Array<any> => [
+        ...prevState,
+        { sender: data.sender, message: data.message },
+      ]);
+    });
+    console.log("Reloaded")
+    return () => {
+      pusher.unsubscribe("chat");
+    };
+  }, [pusher]);
+
+  const handleSignOut = () => {
+    pusher.unsubscribe("chat");
+    router.push("/");
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (messageToSend.trim().length < 1) {
+      return;
+    }
+    await axios.post("/api/pusher", {
+      message: messageToSend,
+      sender,
+    });
+    setMessageToSend("");
+  };
+
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <p>
+                Hello, <span>{sender}</span>
+              </p>
+              <div>
+                <button
+                  onClick={handleSignOut}
+                >
+                  Sign out
+                </button>
+              </div>
+            </div>
+
+            <div>
+              <h2>You&apos;re online.</h2>
+            </div>
+          </div>
+
+          <div>
+            CHAT BOX
+            <div style={{height: "70vh", width: "100%", overflowY: "scroll", border: "1px solid black"}}>
+              {chats.map((chat, id) => (
+                <div key={id}>
+                  <div>
+                    <b>{chat.sender === sender ? "You" : chat.sender}</b> : {chat.message.replaceAll("\\n", "\n")}
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <SendMessage
+                message={messageToSend}
+                handleMessageChange={(e: any) => setMessageToSend(e.target.value)}
+                handleSubmit={handleSubmit}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
