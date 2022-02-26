c73f2a0c08778f279f565c2b2d35b336c2f99548
PullStackPigDev <s300099227@ddsbstudent.ca>
2022-02-26 12:09:57 -0500

    hmm

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@
 module.exports = {
   trailingSlash: true,
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/debug",
-        destination: "http://localhost:3334"
-      },
-      {
-        source: "/debug/:match*",
-        destination: "http://localhost:3334/:match*"
-      }
-    ]
-  }
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/debug",
+  //       destination: "http://localhost:3334"
+  //     },
+  //     {
+  //       source: "/debug/:match*",
+  //       destination: "http://localhost:3334/:match*"
+  //     }
+  //   ]
+  // }
 }
