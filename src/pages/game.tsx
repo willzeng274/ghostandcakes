12bd571cecc322288e7eeb29785947f6bfe0e56a
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2022-03-03 16:38:46 -0500

    hehehehe

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -125,10 +125,10 @@ const Game: NextPage = ({ items }: InferGetServerSidePropsType<typeof getServerS
     console.log(dispatch({type: "INCREMENT", payload: {value: 1}}));
   }, [dispatch]);
   useInterval(function () {
-    if (Math.random() > 0.90) {
+    if (Math.random() > 0.95) {
       setCake2Vis(true);
     }
-  }, 1000);
+  }, 200);
   useInterval(function() {
     if (over || !start) return;
     const [w, h] = getViewport();
