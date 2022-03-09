86aaf333a3891699195b3127d6467556eff473d9
PullStackPigDev <s300099227@ddsbstudent.ca>
2022-03-08 19:46:07 -0500

    google bot test

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="robots" content="all" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,9 +1,20 @@
+import React from 'react'
 import type { NextPage } from 'next'
 
 const Display: NextPage = () => {
+    const [token, setToken] = React.useState<string>("");
+    React.useEffect((): any => {
+        const tok = localStorage.getItem("token") || "testvalue123"
+        console.log(tok);
+        if (tok) setToken(tok);
+        localStorage.removeItem("token");
+        window.addEventListener("beforeunload", () => {
+            if (tok) localStorage.setItem("token", tok)
+        })
+    }, [])
     return (
         <>
-            {"No"}
+            {token}
         </>
     )
 }
